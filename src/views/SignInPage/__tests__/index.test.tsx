d1fef87e65a0ce0bfba5ba3425de317cdaa574d7
Nicholas Bingham <nbingham09@gmail.com>
2022-01-02 11:29:35 -0600

    went to async await for better longevity

--- src/views/SignInPage/__tests__/index.test.tsx
+++ src/views/SignInPage/__tests__/index.test.tsx
@@ -1,20 +1,41 @@
-import { screen } from "@testing-library/react";
-import { render } from "../../../test-utils";
+import { fireEvent, render, screen } from "test-utils";
 
 import { SignInPage, SIGN_IN_PAGE_TEST_IDS } from "..";
 
 describe("SignInPage", () => {
-  it("should render", () => {
+  it("should render", async () => {
     render(<SignInPage />);
 
     expect(
-      screen.getByTestId(SIGN_IN_PAGE_TEST_IDS.HEADING)
+      await screen.findByTestId(SIGN_IN_PAGE_TEST_IDS.HEADING)
     ).toBeInTheDocument();
     expect(
-      screen.getByTestId(SIGN_IN_PAGE_TEST_IDS.ADDRESS_TEXT_FIELD)
+      await screen.findByTestId(SIGN_IN_PAGE_TEST_IDS.ADDRESS_TEXT_FIELD)
     ).toBeInTheDocument();
     expect(
-      screen.getByTestId(SIGN_IN_PAGE_TEST_IDS.SIGN_IN_BUTTON)
+      await screen.findByTestId(SIGN_IN_PAGE_TEST_IDS.SIGN_IN_BUTTON)
     ).toBeInTheDocument();
+  });
+
+  it("should disable the sign in button with no text in the TextField", async () => {
+    render(<SignInPage />);
+
+    expect(
+      await screen.findByTestId(SIGN_IN_PAGE_TEST_IDS.SIGN_IN_BUTTON)
+    ).toHaveAttribute("disabled", "");
+  });
+
+  it("should enable the sign in button with text in the TextField", async () => {
+    render(<SignInPage />);
+
+    const addressTextField = await screen.findByTestId(
+      SIGN_IN_PAGE_TEST_IDS.ADDRESS_TEXT_FIELD
+    );
+
+    // fireEvent(addressTextField, "typ");
+
+    expect(
+      await screen.findByTestId(SIGN_IN_PAGE_TEST_IDS.SIGN_IN_BUTTON)
+    ).toHaveAttribute("disabled", "false");
   });
 });
